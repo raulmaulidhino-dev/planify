7f3cf5d3b48c9f56cc0032bf3243c8d0e619cd3f
Raul Maulidhino <raulahmadm314@gmail.com>
2025-01-09 23:59:48 +0700

    feat: Add Sidebar on the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+import Sidebar from './components/Sidebar.jsx'
+
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = newDate => {
+    setDate(newDate);
+  }
+
   return (
     <>
-      <div>
-      </div>
+      <main>
+        <Sidebar date={date} onChange={onChange} />
+      </main>
     </>
   )
 }
